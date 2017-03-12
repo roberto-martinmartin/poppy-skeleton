e76120cd0fc2e0e1319314fb6f72265ab89044ea
Roberto Martín-Martín <rober@rober-ubuntu-laptop>
2017-03-12 17:13:43 +0100

    Minor

--- software/setup.py
+++ software/setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 
-setup(name='poppy-skeleton',
-      version='TO BE COMPLETED',
+setup(name='robotis_mini_poppy',
+      version='0.1',
       packages=find_packages(),
 
       install_requires=['pypot >= 2.11', 'poppy-creature >= 1.8'],
 
-      author='TO BE COMPLETED',
-      author_email='TO BE COMPLETED',
-      description='TO BE COMPLETED',
+      author='Roberto Martin-Martin',
+      author_email='rmartinmar@gmail.com',
+      description='Pypot model for Robotis Mini robot',
       url='TO BE COMPLETED',
       license='TO BE COMPLETED',
       )
